4644bb448673f2628477ce76235e609435dfb8cc
Gil Creque <gil@codecraftworks.com>
2019-05-22 20:32:30 -0400

    Added header component to app

--- apps/gildotdev/src/app/app.module.ts
+++ apps/gildotdev/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
